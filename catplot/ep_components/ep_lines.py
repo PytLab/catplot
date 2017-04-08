a06282807f24eef949bae8d68f7000aeb455ef05
pytlab <shaozhengjiang@gmail.com>
2017-04-08 23:54:11 +0800

    Add descriptors & ElementaryLine class.

---
+++ catplot/ep_components/ep_lines.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Module for line object in energy profile.
+"""
+
+import catplot.ep_components.descriptors as dc
+from catplot.interpolate import get_potential_energy_points
+
+
+class EPLine(object):
+    """ Base class for lines in energy profile.
+    """
+    def __init__(self, x, y, **kwargs):
+        self.x = x
+        self.y = y
+
+        self.color = kwargs.pop("color", "#000000")
+        self.shadow_color = kwargs.pop("shadow_color", "#595959")
+        self.shadow_depth = kwargs.pop("shadow_depth", 7)
+
+
+class ElementaryLine(EPLine):
+    """ Energy profile line for an elementary reaction.
+    """
+    energies = dc.ElementaryEnergies("energies")
+    rxn_equation = dc.ElementaryReaction("rxn_equation")
+    interp_method = dc.InterpolationMethod("interp_method")
+
+    def __init__(self, energies, **kwargs):
+        self.energies = energies
+
+        # Attributes for basic line.
+        self.n = kwargs.pop("n", 100)
+        self.hline_length = kwargs.pop("hline_length", 1.0)
+        self.peak_width = kwargs.pop("peak_width", 1.0)
+        self.interp_method = kwargs.pop("interp_method", "spline")
+        self.rxn_equation = kwargs.pop("rxn_equation", None)
+
+        # Get x and y lists for the given energies.
+        x, y, _ = get_potential_energy_points(self.energies,
+                                              n=self.n,
+                                              hline_length=self.hline_length,
+                                              peak_width=self.peak_width,
+                                              kind=self.interp_method)
+        super(ElementaryLine, self).__init__(x, y, **kwargs)
+
