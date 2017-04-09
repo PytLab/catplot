239303f80f0ede981e7db75bfe5b0df0d9c80794
pytlab <shaozhengjiang@gmail.com>
2017-04-09 19:21:06 +0800

    Add translate test.

--- tests/elementary_line_test.py
+++ tests/elementary_line_test.py
@@ -47,6 +47,22 @@ class ElementaryLineTest(unittest.TestCase):
         # Check invalid energy tuple.
         self.assertRaises(ValueError, ElementaryLine, [0.0, 1.2, 1.5])
 
+    def test_translate(self):
+        """ Make sure all points in line can be translated correctly.
+        """
+        line = ElementaryLine([0.0, 1.2, 0.7], n=2)
+
+        line.translate(0.5, "x")
+        ref_x = [0.5, 1.5, 1.5, 2.5, 2.5, 3.5]
+        self.assertListEqual(line.x.tolist(), ref_x)
+
+        line.translate(-0.5, "y")
+        ref_y = [-0.5, -0.5, -0.5,
+                 0.19999999999999996,
+                 0.19999999999999996,
+                 0.19999999999999996]
+        self.assertListEqual(line.y.tolist(), ref_y)
+
 if "__main__" == __name__: 
     suite = unittest.TestLoader().loadTestsFromTestCase(ElementaryLineTest)
     unittest.TextTestRunner(verbosity=2).run(suite) 
