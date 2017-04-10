c164ce83462cfca84116f8e21d4699e3d470c082
pytlab <shaozhengjiang@gmail.com>
2017-04-10 16:56:07 +0800

    Add EPLineChain and its test.

--- catplot/ep_components/ep_lines.py
+++ catplot/ep_components/ep_lines.py
@@ -60,6 +60,9 @@ class EPLine(object):
         else:
             raise ValueError("Invalide direction {}".format(direction))
 
+        # Return line itself for the chain operations.
+        return self
+
     def line2d(self):
         """ Create a corresponding matplotlib.lines.Line2D object.
         """
@@ -183,4 +186,37 @@ class ElementaryLine(EPLine):
         EigenPts = namedtuple("EigenPts", ["has_barrier", "A", "B", "C", "D", "E"])
 
         return EigenPts._make([has_barrier, ca, cb, cc, cd, ce])
+
+
+class EPLineChain(EPLine):
+    """ Chain for multiple elementary energy profile lines joined together.
+    """
+    def __init__(self, elementary_lines):
+        self.elementary_lines = elementary_lines
+
+        # Expand all elementary lines.
+        self.expand(self.elementary_lines)
+
+    def __check_elementary_lines(self, lines):
+        for line in lines:
+            if not isinstance(line, ElementaryLine):
+                msg = "Entry in elementary line list must be ElementaryLine obejct."
+                raise ValueError(msg)
+
+    @staticmethod
+    def expand(elementary_lines):
+        """ Expand all elementary lines by translation operations.
+            _
+            _   ->  _ _ _
+            _   ->
+
+        """
+        for idx, line in enumerate(elementary_lines):
+            if idx == 0:
+                continue
+
+            # Translate current line.
+            prev_line = elementary_lines[idx-1]
+            trans_x, trans_y = prev_line.eigen_points.E
+            line.translate(trans_x, "x").translate(trans_y, "y")
 
---
+++ tests/ep_lines_chain_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Test case for ElementaryLine.
+"""
+
+import unittest
+
+from catplot.ep_components.ep_lines import EPLineChain, ElementaryLine
+
+
+class EPLineChainTest(unittest.TestCase):
+
+    def setUp(self):
+        self.maxDiff = True
+
+    def test_construction_and_query(self):
+        """ Test we can construct EPLineChain object correctly.
+        """
+        l1 = ElementaryLine([0.0, 1.2, 0.7])
+        l2 = ElementaryLine([0.0, 1.0, 0.5])
+        l = EPLineChain([l1, l2])
+
+        # Now the l2 should be translated.
+        self.assertTupleEqual(l2.eigen_points.A, (3.0, 0.69999999999999996))
+
+if "__main__" == __name__: 
+    suite = unittest.TestLoader().loadTestsFromTestCase(EPLineChainTest)
+    unittest.TextTestRunner(verbosity=2).run(suite) 
+
