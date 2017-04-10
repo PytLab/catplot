94d110cb424b2132b9a233745af6b1f1a3f21901
pytlab <shaozhengjiang@gmail.com>
2017-04-10 14:44:13 +0800

    Add more tests.

--- tests/ep_canvas_test.py
+++ tests/ep_canvas_test.py
@@ -7,6 +7,7 @@
 import unittest
 
 from catplot.ep_components.ep_canvas import EPCanvas
+from catplot.ep_components.ep_lines import ElementaryLine
 
 
 class EPCanvasTest(unittest.TestCase):
@@ -27,6 +28,31 @@ class EPCanvasTest(unittest.TestCase):
 
         # Check invalid reaction equation.
         self.assertRaises(ValueError, EPCanvas, margin_ratio=-0.1)
+
+    def test_draw(self):
+        """ Make sure the lines can be added without exceptions.
+        """
+        canvas = EPCanvas()
+        line = ElementaryLine([0.0, 1.3, 0.8])
+        canvas.add_lines([line])
+        canvas.draw()
+
+    def test_add_species_annotations(self):
+        """ Make sure the species annotations can be added without exceptions.
+        """
+        canvas = EPCanvas()
+        line = ElementaryLine([0.0, 1.3, 0.8],
+                              rxn_equation="CO_b + O_b <-> CO-O_2b -> CO2_g + 2*_b")
+        canvas.add_lines([line])
+        canvas.add_species_annotations(line)
+
+    def test_add_horizontal_auxiliary_line(self):
+        """ Make sure the horizontal line can be added without exceptions.
+        """
+        canvas = EPCanvas()
+        line = ElementaryLine([0.0, 1.3, 0.8])
+        canvas.add_lines([line])
+        canvas.add_horizontal_auxiliary_line(line)
 
 if "__main__" == __name__: 
     suite = unittest.TestLoader().loadTestsFromTestCase(EPCanvasTest)
