ea4b9205b1cd2bcfbdb471c8dd71243b5943337a
pytlab <shaozhengjiang@gmail.com>
2017-04-11 14:38:12 +0800

    Add data exporting method to EPChain.

--- catplot/ep_components/ep_chain.py
+++ catplot/ep_components/ep_chain.py
@@ -4,6 +4,8 @@
 """ Module for elementary energy profile line chain.
 """
 
+import numpy as np
+
 
 class EPChain(object):
     """ Chain for multiple elementary energy profile lines joined together.
@@ -19,6 +21,18 @@ class EPChain(object):
             if not isinstance(line, ElementaryLine):
                 msg = "Entry in elementary line list must be ElementaryLine obejct."
                 raise ValueError(msg)
+
+    @property
+    def x(self):
+        """ All x values for lines in chain.
+        """
+        return np.concatenate([line.x for line in self.elementary_lines])
+
+    @property
+    def y(self):
+        """ All y values for lines in chain.
+        """
+        return np.concatenate([line.y for line in self.elementary_lines])
 
     @staticmethod
     def expand(elementary_lines):
@@ -69,6 +83,14 @@ class EPChain(object):
         min_x = self.elementary_lines[-0].eigen_points.A[0]
 
         return max_x - min_x
+
+    def export(self, filename):
+        """ Export line data to file.
+        """
+        with open(filename, "w") as f:
+            writer = csv.writer(f)
+            for data in zip(self.x, self.y):
+                writer.writerow(data)
 
     # -------------------------------------------------------------------------
     # Magic method to change default behaviours.
