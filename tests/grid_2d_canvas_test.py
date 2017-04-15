a7696b0cbfa05b99ffdf956c4f063d1a67eecbf2
pytlab <shaozhengjiang@gmail.com>
2017-04-16 00:20:31 +0800

    Add node addition test.

--- tests/grid_2d_canvas_test.py
+++ tests/grid_2d_canvas_test.py
@@ -9,6 +9,7 @@ import unittest
 from matplotlib.collections import PathCollection
 
 from catplot.grid_components.grid_canvas import Grid2DCanvas
+from catplot.grid_components.nodes import Node2D
 
 
 class Grid2DCanvasTest(unittest.TestCase):
@@ -24,6 +25,25 @@ class Grid2DCanvasTest(unittest.TestCase):
         self.assertListEqual(canvas.nodes, [])
         self.assertListEqual(canvas.edges, [])
         self.assertTrue(isinstance(canvas.collection, PathCollection))
+
+    def test_add_node(self):
+        """ Make sure we can add node to canvas correctly.
+        """
+        canvas = Grid2DCanvas()
+
+        n1 = Node2D([0.5, 0.5])
+        n2 = Node2D([1.0, 1.0])
+        canvas.add_node(n1)
+        canvas.add_node(n2)
+
+        # Check nodes in canvas.
+        self.assertTrue(canvas.nodes)
+        for node in canvas.nodes:
+            self.assertTrue(isinstance(node, Node2D))
+
+        # Check cooridnates.
+        ref_coordinates = [[0.5, 0.5], [1.0, 1.0]]
+        self.assertListEqual(ref_coordinates, canvas.node_coordinates.tolist())
 
 if "__main__" == __name__: 
     suite = unittest.TestLoader().loadTestsFromTestCase(Grid2DCanvasTest)
