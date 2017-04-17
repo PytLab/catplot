23ebb916dde541fb71edcdf2f14d0cc07ae0f3b5
pytlab <shaozhengjiang@gmail.com>
2017-04-17 19:39:34 +0800

    Add supercell test.

--- tests/catplot_test.py
+++ tests/catplot_test.py
@@ -11,6 +11,7 @@ from ep_canvas_test import EPCanvasTest
 from elementary_line_test import ElementaryLineTest
 from node_2d_test import Node2DTest
 from edge_2d_test import Edge2DTest
+from supercell_2d_test import SuperCellTest
 
 def suite():
     test_suite = unittest.TestSuite([
@@ -19,6 +20,7 @@ def suite():
         unittest.TestLoader().loadTestsFromTestCase(ElementaryLineTest),
         unittest.TestLoader().loadTestsFromTestCase(Node2DTest),
         unittest.TestLoader().loadTestsFromTestCase(Edge2DTest),
+        unittest.TestLoader().loadTestsFromTestCase(SuperCellTest),
     ])
 
     return test_suite
---
+++ tests/supercell_2d_test.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Test case for supercell.
+"""
+
+import unittest
+
+from catplot.grid_components.nodes import Node2D
+from catplot.grid_components.edges import Edge2D
+from catplot.grid_components.supercell import SuperCell
+
+
+class SuperCellTest(unittest.TestCase):
+
+    def setUp(self):
+        self.maxDiff = True
+
+    def test_construction_and_query(self):
+        """ Test we can construct SuperCell correctly.
+        """
+        node1 = Node2D([1.0, 1.0], color="#595959", width=1)
+        node2 = Node2D([0.5, 0.5], color="#595959", width=1)
+        edge = Edge2D(node1, node2, n=10)
+        supercell = SuperCell([node1, node2], [edge])
+
+    def test_move(self):
+        """ Test the edge can be moved correctly.
+        """
+        node1 = Node2D([1.0, 1.0], color="#595959", width=1)
+        node2 = Node2D([0.5, 0.5], color="#595959", width=1)
+        edge = Edge2D(node1, node2, n=10)
+        supercell = SuperCell([node1, node2], [edge])
+
+        supercell.move([0.5, 0.5])
+
+        self.assertListEqual(node1.coordinate.tolist(), [1.5, 1.5])
+        self.assertListEqual(node2.coordinate.tolist(), [1.0, 1.0])
+
+        ref_x = [1.5,
+                 1.4545454545454546,
+                 1.4090909090909092,
+                 1.3636363636363638,
+                 1.3181818181818181,
+                 1.2727272727272727,
+                 1.2272727272727273,
+                 1.1818181818181819,
+                 1.1363636363636362,
+                 1.0909090909090908,
+                 1.0454545454545454,
+                 1.0]
+
+        self.assertListEqual(edge.x.tolist(), ref_x)
+        self.assertListEqual(edge.y.tolist(), ref_x)
+
+    def test_clone(self):
+        """ Make sure we can clone a new supercell.
+        """
+        node1 = Node2D([1.0, 1.0], color="#595959", width=1)
+        node2 = Node2D([0.5, 0.5], color="#595959", width=1)
+        edge = Edge2D(node1, node2, n=10)
+        supercell = SuperCell([node1, node2], [edge])
+
+        supercell_clone = supercell.clone([0.5, 0.5])
+
+        self.assertListEqual(supercell_clone.nodes[0].coordinate.tolist(),
+                             [1.5, 1.5])
+        self.assertListEqual(supercell_clone.nodes[1].coordinate.tolist(),
+                             [1.0, 1.0])
+
+        ref_x = [1.5,
+                 1.4545454545454546,
+                 1.4090909090909092,
+                 1.3636363636363638,
+                 1.3181818181818181,
+                 1.2727272727272727,
+                 1.2272727272727273,
+                 1.1818181818181819,
+                 1.1363636363636362,
+                 1.0909090909090908,
+                 1.0454545454545454,
+                 1.0]
+
+        self.assertListEqual(supercell_clone.edges[0].x.tolist(), ref_x)
+        self.assertListEqual(supercell_clone.edges[0].y.tolist(), ref_x)
+
+if "__main__" == __name__: 
+    suite = unittest.TestLoader().loadTestsFromTestCase(SuperCellTest)
+    unittest.TextTestRunner(verbosity=2).run(suite) 
+
