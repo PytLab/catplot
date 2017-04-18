3ec71dad9c6aef24c31b07c5a04fd2b2b8c49a29
pytlab <shaozhengjiang@gmail.com>
2017-04-18 20:39:31 +0800

    Add multiple energy profile plot script.

---
+++ scripts/multiplot/plot_multiple_profile.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from catplot.ep_components.ep_lines import ElementaryLine
+from catplot.ep_components.ep_chain import EPChain
+from catplot.ep_components.ep_canvas import EPCanvas
+
+# Read data from input.txt
+globs, locs = {}, {}
+exec(open("input.txt", "r").read(), globs, locs)
+
+# Create chains.
+chains = []
+for idx, (energy_tuples,
+          peak_widths,
+          color,
+          initial_y) in enumerate(zip(locs["multi_energy_tuples"],
+                                      locs["peak_widths"],
+                                      locs["colors"],
+                                      locs["initial_ys"])):
+    lines = []
+    for peak_width, energies in zip(peak_widths, energy_tuples):
+        # Create an elementary line.
+        line = ElementaryLine(energies,
+                              peak_width=peak_width,
+                              color=color,
+                              shadow_depth=locs["shadow_depth"],
+                              shadow_color=locs["shadow_color"])
+        lines.append(line)
+
+    chain = EPChain(lines)
+    chain.translate(initial_y, "y")
+    chains.append(chain)
+
+    # Export data.
+    chain.export("data_{}.csv".format(idx))
+
+# Create energy profile canvas.
+canvas = EPCanvas()
+
+# Add chains to canvas.
+canvas.add_chains(chains)
+canvas.draw()
+
+if locs["display_mode"] == "save":
+    canvas.figure.savefig("multiple_energy_profile.png")
+else:
+    canvas.figure.show()
+
