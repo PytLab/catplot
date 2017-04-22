e7b5861fc1bcec48a41db421bbbbeede49315e89
pytlab <shaozhengjiang@gmail.com>
2017-04-22 16:41:35 +0800

    Add 3d grid canvas class.

--- catplot/grid_components/grid_canvas.py
+++ catplot/grid_components/grid_canvas.py
@@ -4,11 +4,14 @@
 """ Module for grid plotting canvas.
 """
 
+from collections import namedtuple
+
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 from catplot.canvas import Canvas
-from catplot.grid_components.nodes import Node2D
+from catplot.grid_components.nodes import Node2D, Node3D
 from catplot.grid_components.edges import Edge2D, Arrow2D
 from catplot.grid_components.supercell import SuperCell2D
 
@@ -141,7 +144,7 @@ class Grid2DCanvas(Canvas):
         return self._limits(max_x, min_x, max_y, min_y)
 
     def draw(self):
-        """ Draw all nodes and edges on canvas.
+        """ Draw all nodes, edges and arrows on canvas.
         """
         if not any([self.nodes, self.edges, self.arrows]):
             self._logger.warning("Attempted to draw in an empty canvas")
@@ -198,4 +201,110 @@ class Grid2DCanvas(Canvas):
         self.edges = []
         self.arrows = []
         self.supercells = []
+
+
+class Grid3DCanvas(Grid2DCanvas):
+    """ Canvas for 3D grid plotting.
+    """
+    def __init__(self, **kwargs):
+        # NOTE: here we call the method in Canvas NOT Grid2DCanvas.
+        super(Grid2DCanvas, self).__init__(**kwargs)
+
+        # Create a figure.
+        self.figure = plt.figure(figsize=self.figsize,
+                                 dpi=self.dpi)
+
+        # Add an axes to figure.
+        self.axes = self.figure.add_subplot(111, projection="3d",
+                                            facecolor=self.facecolor)
+
+        # Attributes for 3D canvas.
+        self.nodes = []
+        self.edges = []
+        self.arrows = []
+        self.supercells = []
+
+    def _limits(self, max_x, min_x, max_y, min_y, max_z, min_z):
+        """ Override parent's function to get 3D data limits.
+
+        Parameters:
+        -----------
+        max_x: float, the maximum of x values.
+        min_x: float, the minimum of x values.
+        max_y: float, the maximum of y values.
+        min_y: float, the minimum of y values.
+        max_z: float, the maximum of z values.
+        min_z: float, the minimum of z values.
+        """
+        scale_x = max_x - min_x
+        scale_y = max_y - min_y
+        scale_z = max_z - min_z
+
+        # Define a namedtuple to be returned.
+        Limits = namedtuple("Limits", ["max_x", "min_x", "max_y", "min_y", "max_z", "min_z"])
+        limits = [max_x + self.margin_ratio*scale_x,
+                  min_x - self.margin_ratio*scale_x,
+                  max_y + self.margin_ratio*scale_y,
+                  min_y - self.margin_ratio*scale_y,
+                  max_z + self.margin_ratio*scale_z,
+                  min_z - self.margin_ratio*scale_z]
+
+        return Limits._make(limits)
+
+    def _get_data_limits(self):
+        """ Get limits for all data in canvas.
+        """
+        node_x = self.node_coordinates[:, 0] if self.nodes else []
+        edge_x = self.edge_coordinates[:, 0] if self.edges else []
+        arrow_x = self.arrow_coordinates[:, 0] if self.arrows else []
+        x = np.concatenate([node_x, edge_x, arrow_x])
+        max_x, min_x = np.max(x), np.min(x)
+
+        node_y = self.node_coordinates[:, 1] if self.nodes else []
+        edge_y = self.edge_coordinates[:, 1] if self.edges else []
+        arrow_y = self.arrow_coordinates[:, 1] if self.arrows else []
+        y = np.concatenate([node_y, edge_y, arrow_y])
+        max_y, min_y = np.max(y), np.min(y)
+
+        node_z = self.node_coordinates[:, 2] if self.nodes else []
+        edge_z = self.edge_coordinates[:, 2] if self.edges else []
+        arrow_z = self.arrow_coordinates[:, 2] if self.arrows else []
+        z = np.concatenate([node_z, edge_z, arrow_z])
+        max_z, min_z = np.max(z), np.min(z)
+
+        return self._limits(max_x, min_x, max_y, min_y, max_z, min_z)
+
+    def add_node(self, node):
+        """ Add a 3D node to 3D grid canvas.
+        """
+        if not isinstance(node, Node3D):
+            raise ValueError("node must be a Node3D object")
+
+        self.nodes.append(node)
+
+    def draw(self):
+        """ Draw all nodes, edges and arrows on 3D canvas.
+        """
+        if not any([self.nodes, self.edges, self.arrows]):
+            self._logger.warning("Attempted to draw in an empty canvas")
+            return
+
+        # Add nodes to canvas one by one.
+        for node in self.nodes:
+            self.axes.scatter(*node.coordinate,
+                              zdir=node.zdir,
+                              s=node.size,
+                              c=node.color,
+                              depthshade=node.depthshade,
+                              edgecolor=node.edgecolor,
+                              marker=node.style,
+                              alpha=node.alpha,
+                              linewidth=node.line_width,
+                              zorder=node.zorder)
+
+        # Set axes limits.
+        limits = self._get_data_limits()
+        self.axes.set_xlim(limits.min_x, limits.max_x)
+        self.axes.set_ylim(limits.min_y, limits.max_y)
+        self.axes.set_zlim(limits.min_z, limits.max_z)
 
