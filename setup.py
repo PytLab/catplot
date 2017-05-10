1540ac391f97e7499ecb1183773757a81413b9c5
pytlab <shaozhengjiang@gmail.com>
2017-05-10 13:51:44 +0800

    Update version number.

--- setup.py
+++ setup.py
@@ -16,7 +16,47 @@ install_requires = [
 ]
 
 license = 'LICENSE'
-long_description = open('README.rst').read()
+long_description = """
+=======
+catplot
+=======
+
+.. image:: https://travis-ci.org/PytLab/catplot.svg?branch=master
+    :target: https://travis-ci.org/PytLab/catplot
+    :alt: Build Status
+
+.. image:: https://img.shields.io/badge/python-3.5-green.svg
+    :target: https://www.python.org/downloads/release/python-351/
+    :alt: platform
+
+.. image:: https://img.shields.io/badge/python-2.7-green.svg
+    :target: https://www.python.org/downloads/release/python-2710
+    :alt: platform
+
+.. image:: https://img.shields.io/badge/pypi-v1.2.7-blue.svg
+    :target: https://pypi.python.org/pypi/catplot/
+    :alt: versions
+
+.. image:: https://img.shields.io/badge/license-MIT-blue.svg
+    :target: https://raw.githubusercontent.com/PytLab/catplot/master/LICENSE
+
+
+Introduction
+------------
+
+**CatPlot** is a Python Library for Energy Profile and Abstract Grid(2D/3D) plotting.
+
+Installation
+------------
+
+1. Via pip (recommend)::
+
+    pip install catplot
+
+2. From source::
+
+    python setup.py install
+"""
 name = 'catplot'
 packages = [
     'catplot',
