660b22c411894046d92ef65b6833a4c9ff5c99c2
pytlab <shaozhengjiang@gmail.com>
2017-05-12 14:28:04 +0800

    Update version number.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ catplot
     :target: https://www.python.org/downloads/release/python-2710
     :alt: platform
 
-.. image:: https://img.shields.io/badge/pypi-v1.3.0-blue.svg
+.. image:: https://img.shields.io/badge/pypi-v1.3.1-blue.svg
     :target: https://pypi.python.org/pypi/catplot/
     :alt: versions
 
