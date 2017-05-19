6ee6bb01a25bdfa1bc64eeca827b9558d8b7e8ab
pytlab <shaozhengjiang@gmail.com>
2017-05-19 10:56:32 +0800

    Update version number.

--- setup.py
+++ setup.py
@@ -25,26 +25,20 @@ catplot
     :target: https://travis-ci.org/PytLab/catplot
     :alt: Build Status
 
-.. image:: https://img.shields.io/badge/python-3.5-green.svg
+.. image:: https://landscape.io/github/PytLab/catplot/master/landscape.svg?style=flat
+   :target: https://landscape.io/github/PytLab/catplot/master
+   :alt: Code Health
+
+.. image:: https://img.shields.io/badge/python-3.5, 2.7-green.svg
     :target: https://www.python.org/downloads/release/python-351/
     :alt: platform
 
-.. image:: https://img.shields.io/badge/python-2.7-green.svg
-    :target: https://www.python.org/downloads/release/python-2710
-    :alt: platform
-
-.. image:: https://img.shields.io/badge/pypi-v1.3.2-blue.svg
+.. image:: https://img.shields.io/badge/pypi-v1.3.3-blue.svg
     :target: https://pypi.python.org/pypi/catplot/
     :alt: versions
 
 .. image:: https://img.shields.io/badge/license-MIT-blue.svg
     :target: https://raw.githubusercontent.com/PytLab/catplot/master/LICENSE
-
-.. image:: https://img.shields.io/github/stars/PytLab/catplot.svg
-    :target: https://github.com/PytLab/catplot/stargazers
-
-.. image:: https://img.shields.io/github/forks/PytLab/catplot.svg
-    :target: https://github.com/PytLab/catplot/network
 
 Introduction
 ------------
