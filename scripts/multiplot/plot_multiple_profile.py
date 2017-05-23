0d39c69e3b381dee08fb1c03fd75e0b791f51a6f
pytlab <shaozhengjiang@gmail.com>
2017-05-23 16:27:35 +0800

    Add settings for figure size and line width.

--- scripts/multiplot/plot_multiple_profile.py
+++ scripts/multiplot/plot_multiple_profile.py
@@ -11,6 +11,8 @@ from catplot.ep_components.ep_canvas import EPCanvas
 globs, locs = {}, {}
 exec(open("input.txt", "r").read(), globs, locs)
 
+line_width = locs.get("line_width", 3)
+
 # Create chains.
 chains = []
 for idx, (energy_tuples,
@@ -25,6 +27,7 @@ for idx, (energy_tuples,
         # Create an elementary line.
         line = ElementaryLine(energies,
                               peak_width=peak_width,
+                              line_width=line_width,
                               color=color,
                               shadow_depth=locs["shadow_depth"],
                               shadow_color=locs["shadow_color"])
@@ -38,19 +41,22 @@ for idx, (energy_tuples,
     filename = "data_{}.csv".format(idx)
     chain.export(filename)
 
-    print("Write data for chain_{} to {}...".format(idx, filename))
+    print("INFO: write data for chain_{} to {}".format(idx, filename))
 
 # Create energy profile canvas.
-canvas = EPCanvas()
+figsize = locs.get("figsize", (30, 20))
+dpi = locs.get("dpi", 50)
+canvas = EPCanvas(figsize=figsize, dpi=dpi)
 
 # Add chains to canvas.
 canvas.add_chains(chains)
 canvas.draw()
 
-print("Drawing in canvas...")
+print("INFO: drawing in canvas")
 
 if locs["display_mode"] == "save":
     canvas.figure.savefig("multiple_energy_profile.png")
+    print("INFO: energy profile is plotted in multiple_energy_profile.png")
 else:
     canvas.figure.show()
 
